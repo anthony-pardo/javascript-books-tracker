7f26d3800da239f85b1fd82e4914685e1634d52c
Anthony <pardo.anthony@gmail.com>
2020-07-30 07:57:48 -0500

    removed backend

--- backend/app/serializers/habit_serializer.rb
+++
@@ -1,5 +0,0 @@
-class HabitSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :title, :frequency, :goal_date
-  has_many :pomodoros
-end
--- backend/app/serializers/pomodoro_serializer.rb
+++
@@ -1,5 +0,0 @@
-class PomodoroSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :length, :completed
-  belongs_to :habit
-end
